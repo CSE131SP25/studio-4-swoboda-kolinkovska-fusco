548824859cb7961ca90c5ea18901cd7e65472dcb
benlee <benlee@localhost>
2025-02-19 15:13:59 -0600

    dbiwaldkwn

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,7 +20,92 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
-		int image = in.nextInt();
-	}
-}
+//--
+        Scanner inputScanner = new Scanner(System.in);
+        
+        // Get file path from user
+        System.out.println("Enter the path of the shape file:");
+        String filePath = inputScanner.nextLine();
+        
+        // Open the file for reading
+        Scanner fileScanner = new Scanner(new File(filePath));
+
+        // Set up the drawing canvas
+        StdDraw.setCanvasSize(800, 800);
+        StdDraw.setXscale(0, 1);
+        StdDraw.setYscale(0, 1);
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // Read and draw shapes
+        while (fileScanner.hasNext()) {
+            String shapeType = fileScanner.next();
+            int red = fileScanner.nextInt();
+            int green = fileScanner.nextInt();
+            int blue = fileScanner.nextInt();
+            boolean isFilled = fileScanner.nextBoolean();
+            
+            StdDraw.setPenColor(new Color(red, green, blue));
+
+            if (shapeType.equals("rectangle")) {
+                double x = fileScanner.nextDouble();
+                double y = fileScanner.nextDouble();
+                double halfWidth = fileScanner.nextDouble();
+                double halfHeight = fileScanner.nextDouble();
+                
+                if (isFilled) {
+                    StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+                } else {
+                    StdDraw.rectangle(x, y, halfWidth, halfHeight);
+                }
+            } else if (shapeType.equals("ellipse")) {
+                double x = fileScanner.nextDouble();
+                double y = fileScanner.nextDouble();
+                double semiMajor = fileScanner.nextDouble();
+                double semiMinor = fileScanner.nextDouble();
+                
+                if (isFilled) {
+                    StdDraw.filledEllipse(x, y, semiMajor, semiMinor);
+                } else {
+                    StdDraw.ellipse(x, y, semiMajor, semiMinor);
+                }
+            } else if (shapeType.equals("triangle")) {
+                double x1 = fileScanner.nextDouble();
+                double y1 = fileScanner.nextDouble();
+                double x2 = fileScanner.nextDouble();
+                double y2 = fileScanner.nextDouble();
+                double x3 = fileScanner.nextDouble();
+                double y3 = fileScanner.nextDouble();
+                
+                double[] xCoords = {x1, x2, x3};
+                double[] yCoords = {y1, y2, y3};
+                
+                if (isFilled) {
+                    StdDraw.filledPolygon(xCoords, yCoords);
+                } else {
+                    StdDraw.polygon(xCoords, yCoords);
--- src/studio4/NorwayMotherOfAllFlags.java
+++ src/studio4/NorwayMotherOfAllFlags.java
@@ -1,5 +1,7 @@
 package studio4;
 
+import java.awt.Color;
+
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
@@ -10,9 +12,31 @@ import support.cse131.DialogBoxes;
  * @author Dennis Cosgrove (http://www.cse.wustl.edu/~cosgroved/)
  */
 public class NorwayMotherOfAllFlags {
-	private static void mother() {
+	
+	public static void mother() {
+        // Set canvas size and scale
+        StdDraw.setCanvasSize(660, 480);  // 22:16 ratio scaled to 660x480
+        StdDraw.setXscale(0, 22);
+        StdDraw.setYscale(0, 16);
+
+        // Draw the red background
+        StdDraw.setPenColor(new Color(186, 12, 47));  // Official Norway red
+        StdDraw.filledRectangle(11, 8, 11, 8);
+
+        // Draw the white cross (vertical and horizontal bars)
+        StdDraw.setPenColor(Color.GREEN);
+        StdDraw.filledRectangle(6, 8, 1, 8);  // Vertical bar
+        StdDraw.filledRectangle(11, 6, 11, 1); // Horizontal bar
+
+        // Draw the blue cross inside the white cross
+        StdDraw.setPenColor(new Color(0, 32, 91));  // Official Norway blue
+        StdDraw.filledRectangle(6, 8, 0.5, 8);  // Vertical bar (inside white)
+        StdDraw.filledRectangle(11, 6, 11, 0.5); // Horizontal bar (inside white)
+        
+        System.out.println("Norwegian flag drawn successfully.");
+	   
 		StdDraw.clear();
-		StdDraw.setPenColor(255, 0, 0);
+        StdDraw.setPenColor(Color.GREEN);
 		StdDraw.filledRectangle(3, 3, 3, 3);
 		StdDraw.filledRectangle(3, 13, 3, 3);
 		StdDraw.filledRectangle(16, 3, 6, 3);
@@ -21,6 +45,9 @@ public class NorwayMotherOfAllFlags {
 		StdDraw.filledRectangle(11, 8, 11, 1);
 		StdDraw.filledRectangle(8, 8, 1, 8);
 	}
+	
+
+   
 
 	public static void norway() {
 		StdDraw.setXscale(0, 22);
